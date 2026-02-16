bb4591d0b6799cce215071691e9d26cd85c147e5
Niko12126UCN <150657998+Niko12126UCN@users.noreply.github.com>
2026-02-16 11:16:02 +0100

    new test

--- bad-project.tests/CalculatorTests.cs
+++ bad-project.tests/CalculatorTests.cs
@@ -8,4 +8,22 @@ public class CalculatorTests
         var calc = new Calculator();
         Assert.Equal(4, calc.Add(2, 2)); // Fejlen her: 2+2=4, ikke 5
     }
+
+    [Fact]
+    public void AddMultipleNumbers()
+    {
+        var calc = new Calculator();
+
+        var currentSum = 0;
+
+        for (int i = 0; i < 6; i++)
+        {
+            int number = 3;
+            currentSum = calc.Add(number, currentSum);
+        }
+
+        Assert.Equal(18, currentSum);
+    }
+
+
 }
